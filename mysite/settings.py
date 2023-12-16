8d6d4996a0fe66e5fed1a47bae0bf87571bff67e
Matt Calvert <xander.raymatt@gmail.com>
2023-12-16 21:57:54 +0000

    building 404

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-95z_)^i+lk-=q@*&ay215(5ybommtf0@wd2$%ctc-1#m*vmm+o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
